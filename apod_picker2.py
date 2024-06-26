e1ab68e8614a9af57146f1cb22121e215c78a7f0
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-06-26 07:56:17 -0500

    fixes
    no longer hanging, fixed save function, removed old code

--- apod_picker2.py
+++ apod_picker2.py
@@ -54,53 +54,6 @@ def fetch_apod_data():
     messagebox.showerror("Error",f"Failed to fetch APOD data: {e}")
   return None, None
 
-img_url, trash = fetch_apod_data() 
-# Request the image and open it with PIL
-image_response = requests.get(img_url)
-image = Image.open(BytesIO(image_response.content))
-
-""" # Adjust messagebox height if it exceeds screen height
-if image.height > h:
-  max_description_height = h - image.height - 100 # Adjust the padding as needed
-  max_description_lines = max_description_height // 20 # Assuming each line is around 20 pixels
-lines = description.splitlines()
-concatenated_description = ''
-current_line_length = 0
-for line in lines:
-  line = line.strip() # Remove whitespace at the beginning and end of the line
-  if line.startswith('Explanation:' + '\n'):
-    concatenated_description += '\n' + line
-    current_line_length = len(line)
-  else:
-    line = line.replace('\n', ' ') # Replace line breaks within the line with a space
-    if current_line_length + len(line) > w // 10:
-      concatenated_description += '\n' + line
-      current_line_length = len(line)
-    else:
-      concatenated_description += line
-      current_line_length += len(line) """
-
-# # Display the image in a preview window
-# root = tk.Tk()
-# root.title('APOD Image Preview')
-
-# # Set the width of the scrolledtext widget to screen width
-# scroll_text = scrolledtext.ScrolledText(root,
-# width=w // 10, height=10, wrap=tk.WORD)
-# scroll_text.pack(fill=tk.BOTH, expand=True)
-# scroll_text.insert(tk.END, concatenated_description)
-
-# # Create a label and display the image
-# image_label = tk.Label(root)
-# image_label.pack()
-# photo = ImageTk.PhotoImage(image)
-# image_label.configure(image=photo)
-
-# # Prompt user to confirm before setting the image as desktop background
-# messagebox.showinfo('Image Preview', f'Image URL: {img_url}')
-# response = messagebox.askquestion(
-#     'Set Desktop Background', 'Set this image as your desktop background?')
-
 def set_desktop_background(image_path):
   try:  
     if platform.system() == 'Linux':
@@ -125,8 +78,6 @@ def set_desktop_background(image_path):
     messagebox.showerror("Error", f"Failed to set the desktop background: {e}")
 
 def select_save_path(input):
-  root = tk.Tk()
-  root.withdraw()
   file_path = filedialog.asksaveasfilename(defaultextension='.jpg', filetypes=[("JPEG","*.jpg"),("All files","*.*")])
   if file_path:
     try:
@@ -168,6 +119,7 @@ def main():
     image_path = select_save_path(image)
     if image_path:
       set_desktop_background(image_path)
+      image.close()
   else:
     messagebox.showinfo('Set Background Declined','Desktop background has not been changed.')
   
