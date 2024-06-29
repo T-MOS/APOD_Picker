33af9b7f67521b04e8ea757550eee3c820df50df
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-06-29 14:10:17 -0500

    Update json_tracker.py
    first ver of config default save dir initializer

--- test/json_tracker.py
+++ test/json_tracker.py
@@ -3,6 +3,21 @@ import os
 
 jsonPath = "test\\savesList.json"
 
+def default_dir_initializer():
+  # open/read config
+  with open(jsonPath, 'r') as f:
+    configObj = json.load(f)
+  # check if default is initialized
+  if configObj['default_dir_path'] == "":
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    default_relative_path = os.path.join(current_dir, 'test')
+  with open(jsonPath, 'w') as f:
+    configObj = json.load(f)
+    configObj['default_dir_path'] = default_relative_path
+    json.dump(configObj, f, indent=2)
+  return default_relative_path
+# default_dir_initializer()
+
 def update_config(saved):
   with open(jsonPath, 'r') as f:
     configObj = json.load(f)
@@ -18,5 +33,4 @@ def update_config(saved):
 
   with open(jsonPath, 'w') as out:
     json.dump(configObj, out, indent=2)
-
-update_config('path/')
+# update_config('path/')
