613bd2d1ec81de82892b0860abce03a11515ddd4
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-06-28 22:10:59 -0500

    Update json_tracker.py

--- test/json_tracker.py
+++ test/json_tracker.py
@@ -9,12 +9,12 @@ def update_config(saved):
   
   paths = configObj['paths']
 
-  if len(paths) == configObj['max']:
+  if len(paths) >= configObj['keep']:
     oldest = configObj['paths'][-1]
     if os.path.exists(oldest):
       os.remove(oldest)
-      paths.insert(0,saved)
-      paths = paths[:5]
+  paths.insert(0,saved)
+  configObj['paths']=paths[:5]
 
   with open(jsonPath, 'w') as out:
     json.dump(configObj, out, indent=2)
