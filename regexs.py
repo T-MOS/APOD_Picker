8a048ec18ac9a8120b8b14c3dc388f41c380f7a8
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-16 16:59:16 -0500

    Update regexs.py

--- regexs.py
+++ regexs.py
@@ -1,10 +1,34 @@
 import re
 
-dummyString = "Apple M1:\r\n\nChipset Model: Apple M1\r\n      Type: GPU\r\n      Bus: Built-In\r\n      Total Number of Cores: 8\r\n      Vendor: Apple (0x106b)\r\n      Metal Support: Metal 3\r\n      Displays:\r\n        Color LCD:\r\n          Display Type: Built-In Retina LCD\r\n          Resolution: 2560 x 1600 Retina\r\n          Main Display: Yes\r\n          Mirror: Off\r\n          Online: Yes\r\n          Automatically Adjust Brightness: Yes\r\n          Connection Type: Internal\r\n        Sidecar Display:\r\n          Resolution: 2732 x 2048\r\n          UI Looks like: 1366 x 1024 @ 60.00Hz\r\n          Framebuffer Depth: 24-Bit Color (ARGB8888)\r\n          Mirror: Off\r\n          Connection Type: AirPlay\r\n          Virtual Device: Yes"
+dummyString = r"""Apple M1:
+
+      Chipset Model: Apple M1
+      Type: GPU
+      Bus: Built-In
+      Total Number of Cores: 8
+      Vendor: Apple (0x106b)
+      Metal Support: Metal 3
+      Displays:
+        Color LCD:
+          Display Type: Built-In Retina LCD
+          Resolution: 2560 x 1600 Retina
+          Main Display: Yes
+          Mirror: Off
+          Online: Yes
+          Automatically Adjust Brightness: Yes
+          Connection Type: Internal
+        Sidecar Display:
+          Resolution: 2732 x 2048
+          UI Looks like: 1366 x 1024 @ 60.00Hz
+          Framebuffer Depth: 24-Bit Color (ARGB8888)
+          Mirror: Off
+          Connection Type: AirPlay
+          Virtual Device: Yes """
 
 def mac_display_info(text):
-  SysParam_pattern = re.compile(r"(\w+ \w+):\r?\n(?:\s+.*?\r?\n)*?\s+Resolution: (\d+ x \d+)(?:.*?\r?\n)*?(?:\s+UI Looks like: (\d+ x \d+))?(?:.*?\r?\n)*?\s+Main Display: (Yes|No)")
-  alt_pattern = 
+  SysParam_pattern = re.compile(r"\s+(\w+ \w+):\r?\n(?:\s+.*?\r?\n)*?\s+Resolution: (\d+ x \d+)(?:.*?\r?\n)*?(?:\s+UI Looks like: (\d+ x \d+))?(?:.*?\r?\n)*?\s+Main Display: (Yes|No)")
+  # Disp. name, wdim x hdim
+  alt_pattern = r"\s+(\w+ \w+): ?\r?\n(?:\s+.*\r?\n)*?\s+Resolution: (\d+ x \d+)"
   matches = SysParam_pattern.findall(text)
   
   displays_list = []
