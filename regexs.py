aeddb9086e20070cf0a6b75aaf72718bdf981881
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-17 13:26:08 -0500

    Update regexs.py

--- regexs.py
+++ regexs.py
@@ -1,5 +1,10 @@
 import re
 
+def sanitize_filename(url_string):
+  pattern = r'([^/]+)\.[^.]+$' #read: "after last '/' before last '.'"
+  rinsed = re.search(pattern, url_string)
+  return rinsed
+
 dummyString = r"""Apple M1:
 
       Chipset Model: Apple M1
@@ -26,10 +31,10 @@ dummyString = r"""Apple M1:
           Virtual Device: Yes """
 
 def mac_display_info(text):
-  SysParam_pattern = re.compile(r"\s+(\w+ \w+):\r?\n(?:\s+.*?\r?\n)*?\s+Resolution: (\d+ x \d+)(?:.*?\r?\n)*?(?:\s+UI Looks like: (\d+ x \d+))?(?:.*?\r?\n)*?\s+Main Display: (Yes|No)")
+  # SysParam_pattern = re.compile(r"\s+(\w+ \w+):\r?\n(?:\s+.*?\r?\n)*?\s+Resolution: (\d+ x \d+)(?:.*?\r?\n)*?(?:\s+UI Looks like: (\d+ x \d+))?(?:.*?\r?\n)*?\s+Main Display: (Yes|No)")
   # Disp. name, wdim x hdim, UI scale(looks like)
   alt_pattern = r"\s+(\w+ \w+): ?\r?\n(?:\s+.*\r?\n)*?\s+Resolution: (\d+ x \d+)(?:.*?\r?\n)*?(?:\s+UI Looks like: (\d+ x \d+))?(?:.*?\r?\n)*?"
-  matches = SysParam_pattern.findall(text)
+  matches = alt_pattern.findall(text)
   
   displays_list = []
   # if len(matches)>1:
@@ -44,7 +49,28 @@ def mac_display_info(text):
     displays_list.append(display_info)
   return matches
 
-def sanitize_filename(url_string):
-  pattern = r'([^/]+)\.[^.]+$' #read: "after last '/' before last '.'"
-  rinsed = re.search(pattern, url_string)
-  return rinsed
+def mac_dual_display(text):
+  pattern = r"\s+(\w+ \w+): ?\r?\n"
+  matches = re.findall(pattern,text)
+  lines = text.splitlines()
+  indices = list()
+
+  # extract the 2 blocks of text corresponding to disp.s' info
+  for line in lines:
+    for i in matches:
+      if i in line:
+        indices.append(lines.index(line))
+  if len(indices) == 2:
+    a = [lines[i] for i in range(indices[0],indices[1])]
+    b = [lines[i] for i in range(indices[1],len(lines))]
+    displays = [a,b]
+  # format the needed info
+  for i in displays:
+    for line in i:
+      resolutionPattern = r"\d+[^ x]\d+"
+      resolutionTuple = []
+      # print(line)
+      if "Resolution:" in line:
+        digits = re.findall(resolutionPattern,line)
+        resolutionTuple = tuple(digits)
+        print(resolutionTuple)
