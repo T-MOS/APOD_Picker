4cc3a7b0fe4da179b4b9c42b742bdb0cdabc3a1e
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-17 16:45:19 -0500

    Update AppleScript Alternates.py

--- test/AppleScript Alternates.py
+++ test/AppleScript Alternates.py
@@ -1,34 +1,36 @@
 import os
-# 'Darwin':
+#old
 script0 = f"""
 tell application "Finder"
   set desktop picture to POSIX file "{path}"
 end tell
 """
+#oneliner old
 script0_1 = f"""
 tell application "Finder" to set desktop picture to POSIX file "{path}"
 """
 
 
+#finder
 script1 = f"""
-tell application "System Events"
+tell application "Finder"
   tell desktop 2
     set desktop picture to POSIX file "{path2}"
   end tell
 end tell
 """
 script1_1 = f"""
-tell application "System Events" to tell desktop 2 to set desktop picture to POSIX file "{path2}" end tell
+tell application "Finder" to tell desktop 2 to set desktop picture to POSIX file "{path2}" end tell
 """
 
+#sys evt
 script2 = f"""
-tell application "Finder"
+tell application "System Events"
   tell desktop 2
     set desktop picture to POSIX file "{path2}"
   end tell
 end tell
 """
 script2_1 = f"""
-tell application "Finder" to tell desktop 2 to set desktop picture to POSIX file "{path2}" end tell
+tell application "System Events" to tell desktop 2 to set desktop picture to POSIX file "{path2}" end tell
 """
-
