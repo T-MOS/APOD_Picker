ce21c89785f988b2048cbb78aa3d1db253057daf
T-MOS <50530796+T-MOS@users.noreply.github.com>
2024-08-20 15:02:33 -0500

    Update APOD Picker.py

--- APOD Picker.py
+++ APOD Picker.py
@@ -10,7 +10,7 @@ import tempfile
 import scheduler
 import re
 from log import to_errlog, json_log
-from Mac_specifics import *
+from Mac_specifics import mac_dual_display
 from screeninfo import get_monitors
 from tkinter import Tk
 from datetime import datetime
